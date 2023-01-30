02a6c958d9234d1b34409bc7b62ec724dfc3a355
听日要早起 <xujinhuan_email@163.com>
2023-01-30 21:04:42 +0800

    feat:complete the zuhe

--- algorithms/huisu/01_zuhe.go
+++ algorithms/huisu/01_zuhe.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 // 问题：给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
 /*
 	示例:
@@ -125,6 +127,90 @@ func LetterCombinations(digits string) []string {
 		}
 	}
 	dfs(digits,0,path1)
+	return res
+}
+/*
+	给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+	candidates 中的数字可以无限制重复被选取。
+
+	说明：
+
+    所有数字（包括 target）都是正整数。
+    解集不能包含重复的组合。
+
+	示例 1： 输入：candidates = [2,3,6,7], target = 7, 所求解集为： [ [7], [2,2,3] ]
+
+	示例 2： 输入：candidates = [2,3,5], target = 8, 所求解集为： [   [2,2,2,2],   [2,3,3],   [3,5] ]
+*/
+// CombinationSum
+func CombinationSum(candidates []int, target int) [][]int {
+	res := make([][]int,0)
+
+	var dfs func(candidates []int,start,target int,path []int)
+	dfs = func(candidates []int,start,target int,path []int) {
+		if target == 0{
+			temp := make([]int,len(path))
+			copy(temp,path)
+			res = append(res,temp)
+			return
+		}
+		for i:=start;i<len(candidates);i++{
+			if candidates[i] > target {  // 剪枝，提前返回
+				break
+			}
+			path = append(path,candidates[i])
+			dfs(candidates,i,target-candidates[i],path)
+			path = path[:len(path) - 1]
+		}
+	}
+	path := make([]int,0)
+	sort.Ints(candidates)
+	dfs(candidates,0,target,path)
+	return res
+}
+
+/*
+	给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+	candidates 中的每个数字在每个组合中只能使用一次。
+
+	说明： 所有数字（包括目标数）都是正整数。 解集不能包含重复的组合。
+
+	示例 1: 输入: candidates = [10,1,2,7,6,1,5], target = 8, 所求解集为: [ [1, 7], [1, 2, 5], [2, 6], [1, 1, 6] ]
+
+	示例 2: 输入: candidates = [2,5,2,1,2], target = 5, 所求解集为: [   [1,2,2],   [5] ]
+*/
+// CombinationSum2 40. 组合总和 II
+func CombinationSum2(candidates []int, target int) [][]int {
+	res := make([][]int,0)
+
+	var dfs func(candidates []int,start,target int,path []int)
+	dfs = func(candidates []int,start,target int,path []int) {
+		if target == 0{
+			temp := make([]int,len(path))
+			copy(temp,path)
+			res = append(res,temp)
+			return
+		}
+		numUsed := make(map[int]bool)
+		for i:=start;i<len(candidates);i++{
+			if candidates[i] > target{
+				break
+			}
+			if _,ok := numUsed[candidates[i]];ok{
+				continue
+			}else{
+				numUsed[candidates[i]] = true
+			}
+			path = append(path,candidates[i])
+			dfs(candidates,i + 1,target - candidates[i],path)
+			path = path[:len(path)-1]
+		}
+	}
+	sort.Ints(candidates)
+	path := make([]int,0)
+	dfs(candidates,0,target,path)
 	return res
 }
 
