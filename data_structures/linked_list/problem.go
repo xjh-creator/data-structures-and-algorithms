eb865c36833171049ec0df639e7a2f9a0ca2ca65
听日要早起 <xujinhuan_email@163.com>
2023-04-05 10:28:05 +0800

    feat:add two problem for linked_list

--- data_structures/linked_list/problem.go
+++ data_structures/linked_list/problem.go
@@ -123,6 +123,24 @@ func (this *MyLinkedList) DeleteAtIndex(index int)  {
  * obj.DeleteAtIndex(index);
  */
 
+/*
+	题意：反转一个单链表。
+
+	示例: 输入: 1->2->3->4->5->NULL 输出: 5->4->3->2->1->NULL
+*/
+// reverseList 206.反转链表
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.next
+		cur.next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
+
 func RcReverseList(head *ListNode) *ListNode {
 	return solve(nil,head)
 }
@@ -133,9 +151,7 @@ func solve(pre,cur *ListNode) *ListNode {
 	}
 	next := cur.next
 	cur.next = pre
-	pre = cur
-	cur = next
-	return solve(pre,cur)
+	return solve(cur,next)
 }
 
 func PrintListNode(head *ListNode)  {
@@ -143,4 +159,65 @@ func PrintListNode(head *ListNode)  {
 		fmt.Printf("%d-->",head.value.(int))
 		head = head.next
 	}
+}
+
+/*
+	给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。
+	你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+*/
+
+// swapPairs 24. 两两交换链表中的节点
+func swapPairs(head *ListNode) *ListNode {
+	temp := &ListNode{
+		next:head,
+	}
+
+	pre := temp
+	for head != nil && head.next != nil{
+		next := head.next.next
+		pre.next = head.next
+		head.next.next = head
+		head.next = next
+
+		pre = head
+		head = head.next
+	}
+
+	return temp.next
+}
+
+/*
+	给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+*/
+
+// removeNthFromEnd 19. 删除链表的倒数第 N 个结点
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	pre := &ListNode{
+		next:head,
+	}
+
+	cur := head
+	for i:=0;i<n;i++{
+		cur = cur.next
+	}
+
+	// 删除链表只有一个值且删除倒数第一个节点的值
+	if cur == nil && n == 1{
+		return nil
+	}
+
+	step := 0 // 计算 pre 移动的次数
+	for cur != nil{
+		cur = cur.next
+		pre = pre.next
+		step++
+	}
+
+	pre.next = pre.next.next
+
+	if step == 0{ // pre 没有移动，则返回其下一个节点，即新头节点
+		return pre.next
+	}
+
+	return head
 }
