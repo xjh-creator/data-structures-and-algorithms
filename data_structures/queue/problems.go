693fd91f0fd1776da857b9ee825c289782463f61
听日要早起 <xujinhuan_email@163.com>
2023-04-22 08:51:14 +0800

    add 20 isVaild

--- data_structures/queue/problems.go
+++ data_structures/queue/problems.go
@@ -1,5 +1,4 @@
 package queue
-
 /*
 	使用队列实现栈的下列操作：
 
@@ -8,95 +7,62 @@ package queue
     top() -- 获取栈顶元素
     empty() -- 返回栈是否为空
 
-*/
+注意:
 
+    你只能使用队列的基本操作-- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的。
+    你所使用的语言也许不支持队列。 你可以使用 list 或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+    你可以假设所有操作都是有效的（例如, 对一个空的栈不会调用 pop 或者 top 操作）。
+
+*/
+// 225. 用队列实现栈
 type MyStack struct {
-	queue *ArrayQueue
+	//创建两个队列
+	queue1 []int
+	queue2 []int
 }
 
-func NewMyStack(n int) *MyStack {
-	return &MyStack{
-		queue: NewArrayQueue(n),
+
+func Constructor() MyStack {
+	return MyStack{	//初始化
+		queue1:make([]int,0),
+		queue2:make([]int,0),
 	}
 }
 
-func (m *MyStack)Push(e int) bool {
-	return m.queue.EnQueue(e)
+
+func (this *MyStack) Push(x int)  {
+	//先将数据存在queue2中
+	this.queue2 = append(this.queue2,x)
+	//将queue1中所有元素移到queue2中，再将两个队列进行交换
+	this.Move()
 }
 
-func (m *MyStack)Pop() int {
-   return m.queue.DeQueue().(int)
-}
 
-func (m *MyStack)Top() int {
-	return m.queue.Top().(int)
-}
-
-func (m *MyStack)Empty() bool {
-	return m.queue.head == m.queue.tail
-}
-
-/*
-	给定一个数组 nums，有一个大小为k的滑动窗口从数组的最左侧移动到数组的最右侧。
-
-	你只可以看到在滑动窗口内的 k个数字。滑动窗口每次只向右移动一位。
-
-	返回滑动窗口中的最大值。
-*/
-// MaxSlidingWindow 滑动窗口最大值
-type MyQueue struct {
-	queue []int
-}
-
-func NewMyQueue() *MyQueue {
-	return &MyQueue{
-		queue: make([]int, 0),
+func (this *MyStack) Move(){
+	if len(this.queue1) == 0{
+		//交换，queue1置为queue2,queue2置为空
+		this.queue1,this.queue2 = this.queue2,this.queue1
+	}else{
+		//queue1元素从头开始一个一个追加到queue2中
+		this.queue2 = append(this.queue2,this.queue1[0])
+		this.queue1 = this.queue1[1:]	//去除第一个元素
+		this.Move()     //重复
 	}
 }
 
-func (m *MyQueue) Front() int {
-	return m.queue[0]
+func (this *MyStack) Pop() int {
+	val := this.queue1[0]
+	this.queue1 = this.queue1[1:]	//去除第一个元素
+	return val
+
 }
 
-func (m *MyQueue) Back() int {
-	return m.queue[len(m.queue)-1]
+
+func (this *MyStack) Top() int {
+	return this.queue1[0]	//直接返回
 }
 
-func (m *MyQueue) Empty() bool {
-	return len(m.queue) == 0
-}
 
-func (m *MyQueue) Push(val int) {
-	for !m.Empty() && val > m.Back() {
-		m.queue = m.queue[:len(m.queue)-1]
-	}
-	m.queue = append(m.queue, val)
-}
-
-func (m *MyQueue) Pop(val int) {
-	if !m.Empty() && val == m.Front() {
-		m.queue = m.queue[1:]
-	}
-}
-
-func maxSlidingWindow(nums []int, k int) []int {
-	queue := NewMyQueue()
-	length := len(nums)
-	res := make([]int, 0)
-	// 先将前k个元素放入队列
-	for i := 0; i < k; i++ {
-		queue.Push(nums[i])
-	}
-	// 记录前k个元素的最大值
-	res = append(res, queue.Front())
-
-	for i := k; i < length; i++ {
-		// 滑动窗口移除最前面的元素
-		queue.Pop(nums[i-k])
-		// 滑动窗口添加最后面的元素
-		queue.Push(nums[i])
-		// 记录最大值
-		res = append(res, queue.Front())
-	}
-	return res
+func (this *MyStack) Empty() bool {
+	return len(this.queue1) == 0
 }
--- data_structures/stack/problems.go
+++ data_structures/stack/problems.go
@@ -1,56 +1,95 @@
 package stack
 
-// IsValid ()匹配,s 取值只能为 (){}[]
-func (a *ArrayStack)IsValid(s string) bool {
-	temp := []byte(s)
-	if len(temp)%2 != 0{
-		return false
+/*
+	使用栈实现队列的下列操作：
+
+	push(x) -- 将一个元素放入队列的尾部。
+	pop() -- 从队列首部移除元素。
+	peek() -- 返回队列首部的元素。
+	empty() -- 返回队列是否为空。
+
+	示例:
+
+	MyQueue queue = new MyQueue();
+	queue.push(1);
+	queue.push(2);
+	queue.peek();  // 返回 1
+	queue.pop();   // 返回 1
+	queue.empty(); // 返回 false
+
+	说明:
+
+    你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+    你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+    假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+
+*/
+
+// 232.用栈实现队列
+type MyQueue struct {
+	stackIn  []int //输入栈
+	stackOut []int //输出栈
+}
+
+func Constructor() MyQueue {
+	return MyQueue{
+		stackIn:  make([]int, 0),
+		stackOut: make([]int, 0),
 	}
-	for _,v := range temp{
-		switch v {
-		case '(':
-			a.Push(')')
-		case '{':
-			a.Push('}')
-		case '[':
-			a.Push(']')
-		case ')':
-			if a.Pop() != ')'{
-				return false
-			}
-		case ']':
-			if a.Pop() != ']'{
-				return false
-			}
-		case '}':
-			if a.Pop() != '}'{
-				return false
-			}
-		default:
+}
+
+// 往输入栈做push
+func (this *MyQueue) Push(x int) {
+	this.stackIn = append(this.stackIn, x)
+}
+
+// 在输出栈做pop，pop时如果输出栈数据为空，需要将输入栈全部数据导入，如果非空，则可直接使用
+func (this *MyQueue) Pop() int {
+	inLen, outLen := len(this.stackIn), len(this.stackOut)
+	if outLen == 0 {
+		if inLen == 0 {
+			return -1
+		}
+		for i := inLen - 1; i >= 0; i-- {
+			this.stackOut = append(this.stackOut, this.stackIn[i])
+		}
+		this.stackIn = []int{}      //导出后清空
+		outLen = len(this.stackOut) //更新长度值
+	}
+	val := this.stackOut[outLen-1]
+	this.stackOut = this.stackOut[:outLen-1]
+	return val
+}
+
+func (this *MyQueue) Peek() int {
+	val := this.Pop()
+	if val == -1 {
+		return -1
+	}
+	this.stackOut = append(this.stackOut, val)
+	return val
+}
+
+func (this *MyQueue) Empty() bool {
+	return len(this.stackIn) == 0 && len(this.stackOut) == 0
+}
+
+// isValid 20. 有效的括号
+func isValid(s string) bool {
+	hash := map[byte]byte{')':'(', ']':'[', '}':'{'}
+	stack := make([]byte, 0)
+	if s == "" {
+		return true
+	}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
+			stack = append(stack, s[i])
+		} else if len(stack) > 0 && stack[len(stack)-1] == hash[s[i]] {
+			stack = stack[:len(stack)-1]
+		} else {
 			return false
 		}
 	}
-	if a.top != -1{
-		return false
-	}
-	return true
+	return len(stack) == 0
 }
-
-// RemoveDuplicates 删除字符串中的所有相邻重复项
-func (a *ArrayStack)RemoveDuplicates(s string) string {
-	temp := []rune(s)
-	for _,v := range temp{
-		if a.Top() == nil || v != a.Top().(rune){
-			a.Push(v)
-		}else{
-			a.Pop()
-		}
-	}
-    if a.IsEmpty(){
-    	return ""
-	}
-	return a.GetAllDataInString()
-}
-
-
-
